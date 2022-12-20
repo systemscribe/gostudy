da251ca0c922cf57c33bd783b89624cb52edda13
System Scribe <scribe@DESKTOP-OP46IKM>
2022-12-20 23:21:58 +0200

    set up basic word counter

---
+++ word-counter/wc_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+  "bytes"
+  "testing"
+)
+
+// Test counter
+
+func TestCountWords (t *testing.T) {
+  b := bytes.NewBufferString("word1 word2 word3 word4\n")
+
+  exp := 4
+
+  res:= count(b)
+
+  if res != exp {
+    t.Errorf("Expected %d, got %d instead.\n", exp, res)
+
+
+  }
+}
--- word-counter/word-counter.go
+++ word-counter/word-counter.go
@@ -15,5 +15,17 @@ func main() {
 func count (r io.Reader) int {
   //Scanner for reading text
   scanner := bufio.NewScanner(r)
+  //How the scanner splits
+  scanner.Split(bufio.ScanWords)
+  
+  //Define the counter
+  wc := 0
 
+  for scanner.Scan() {
+
+    wc++
+  }
+
+  //return the counter
+  return wc
 }
